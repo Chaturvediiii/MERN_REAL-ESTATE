2e5ea5d5087c231d2e13b7cb39dcfa8a8c15bb72
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-04-06 12:44:18 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='flex justify-center items-center h-screen text-red-500'>
+      Hello
+    </h1>
+  )
+}
+
