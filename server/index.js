a1acace677f4ce74aaf1443f664895795e886dd3
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-04-06 16:54:57 +0530

    created error middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 dotenv.config()
 
@@ -18,4 +19,15 @@ app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}!!`);
 })
 
+app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.errmsg || 'Internal server error'
+    return res.status(statusCode).json({
+        success : false,
+        message,
+        statusCode
+    })
+})
