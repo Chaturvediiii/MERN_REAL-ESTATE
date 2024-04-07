e47d4c0b69a896194f41e682e29bffa0b8edf1d3
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-04-07 21:30:55 +0530

    google auth functionality

---
+++ client/src/firebase.config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBHBtqYHZLnwxperRCQEP1x7GUQjs56-Pc",
+  authDomain: "mern-estate-d63ec.firebaseapp.com",
+  projectId: "mern-estate-d63ec",
+  storageBucket: "mern-estate-d63ec.appspot.com",
+  messagingSenderId: "1023420393989",
+  appId: "1:1023420393989:web:f2372c3dac60940d0a4a6c"
+};
+
+export const app = initializeApp(firebaseConfig);
