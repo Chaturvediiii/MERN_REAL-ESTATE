259faf1f7281c6be3c6935deca0079f2832edebc
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-04-08 21:12:19 +0530

    till 8.04.2024

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,8 +12,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
+
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 const PORT = 3000;
 
 app.listen(PORT,()=>{
