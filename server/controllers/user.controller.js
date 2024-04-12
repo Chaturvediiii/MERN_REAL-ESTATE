2abaacb118d7d378d43cb7948d36c03458b9a5c3
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-04-12 10:56:48 +0530

    added errorHandler

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs'
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
