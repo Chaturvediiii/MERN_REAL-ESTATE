311fce353cabd50a90c47f1b65e93efe98a4cc66
Tanisha Chaturvedi <tanisha.17012003@gmail.com>
2024-07-05 12:55:29 +0530

    build changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,6 +13,14 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
-  },
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 })
